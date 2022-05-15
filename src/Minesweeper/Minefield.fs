7af4d8544a2f453f78c55e57b6b89a8e1f29fed5
SeungYongShim <syshim@me.com>
2022-05-15 15:01:40 +0900

    .

--- src/Minesweeper/Minefield.fs
+++ src/Minesweeper/Minefield.fs
@@ -13,6 +13,20 @@ module Minefield =
         | Playing (w, h, z) -> z
         | _ -> Map.empty
 
+    let click p v =
+        let map = Option.map
+        let mapClick = map Cell.click
+        let q z =
+            match z.Item p with
+            | Covered _ ->
+                let clicked = z |> Map.change p mapClick
+                match clicked.Item p with
+                | Number 0 -> clicked 
+                | _ -> clicked
+            | _ -> z
+        match v with
+        | Playing (w, h, z) -> Playing (w, h, q z)
+
     let rec start v =
         match v with
         | Setup (w, h) -> Playing (w, h, query {
@@ -22,10 +36,11 @@ module Minefield =
         } |> Map.ofSeq)
         | SetupWithBombPos (w, h, z) ->
             let cells = Setup (w, h) |> start |> getCells
-            let (+) a b = (fst(a) + fst(b), snd(a) + snd(b))
             let cellsWithBombs = (cells, z) ||> Seq.fold (fun acc key ->
-                let mapAdd = Option.map Cell.add
-                acc |> Map.change key (Option.map (fun i -> Covered Bomb))
+                let map = Option.map
+                let mapAdd = map Cell.add
+                let (+) a b = (fst(a) + fst(b), snd(a) + snd(b))
+                acc |> Map.change key (map(fun i -> Covered Bomb))
                     |> Map.change (key + (-1, -1)) mapAdd
                     |> Map.change (key + (-1,  0)) mapAdd
                     |> Map.change (key + (-1,  1)) mapAdd
@@ -39,7 +54,8 @@ module Minefield =
         | _ -> v
 
     let count v =
+        let count = Map.count
         match v with
-        | Playing (w, h, z) -> z |> Map.count
+        | Playing (w, h, z) -> z |> count
         | _ -> 0
    
