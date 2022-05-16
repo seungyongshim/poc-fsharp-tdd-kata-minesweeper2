e9624c1a648e99e9d33bd4fc109b68011cc4214f
SeungYongShim <syshim@me.com>
2022-05-16 23:56:06 +0900

    .

---
+++ src/Minesweeper.Console/Program.fs
@@ -0,0 +1,26 @@
+open System.Security.Cryptography
+open Minesweeper.Cell
+open Minesweeper.Minefield
+open Minesweeper
+open System
+
+let setup width height bombs = SetupWithBombPos(width, height,
+    Seq.initInfinite(fun _ -> (RandomNumberGenerator.GetInt32(height), RandomNumberGenerator.GetInt32(width)))
+    |> Seq.distinct
+    |> Seq.take bombs)
+
+let game = setup 3 3 3 |> start
+
+let rec gameloop g =
+    match g with
+    | Playing _ -> 
+        g |> string |> Console.WriteLine
+        let y = Console.ReadLine() |> Convert.ToInt32
+        let x = Console.ReadLine() |> Convert.ToInt32
+        let n = g |> click (y, x)
+        Console.SetCursorPosition(0, 0)
+        gameloop n
+    | _ -> g |> string |> Console.WriteLine
+
+gameloop game
+    
